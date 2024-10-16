6aed3a57e640dc0c2a258913a9605992ce5b91f7
Daniel Seravalli <daniel@holler.io>
2024-10-16 15:44:54 -0400

    Neotest key bindings

--- lua/dseravalli/plugins/plugins.lua
+++ lua/dseravalli/plugins/plugins.lua
@@ -83,6 +83,7 @@ return {
       local null_ls = require("null-ls")
       null_ls.setup({
         sources = {
+          null_ls.builtins.diagnostics.pylint,
           null_ls.builtins.formatting.isort,
           null_ls.builtins.formatting.black,
         },
