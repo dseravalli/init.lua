f7bcad4d923c9f65f6441c6365a92b4864d55fdb
Daniel Seravalli <daniel@holler.io>
2024-10-22 11:03:05 -0400

    x

--- lua/dseravalli/lazy/init.lua
+++ lua/dseravalli/lazy/init.lua
@@ -3,9 +3,7 @@ return {
   "tpope/vim-rhubarb",
   "szw/vim-maximizer",
   "christoomey/vim-tmux-navigator",
-  "joerdav/templ.vim",
   "HiPhish/jinja.vim",
-  "vrischmann/tree-sitter-templ",
   { "numToStr/Comment.nvim", opts = {} },
   -- "github/copilot.vim",
 }
--- lua/dseravalli/lazy/treesitter.lua
+++ lua/dseravalli/lazy/treesitter.lua
@@ -1,15 +1,17 @@
 return {
   -- Highlight, edit, and navigate code
-  'nvim-treesitter/nvim-treesitter',
+  "nvim-treesitter/nvim-treesitter",
   dependencies = {
-    'nvim-treesitter/nvim-treesitter-textobjects',
+    "joerdav/templ.vim",
+    "vrischmann/tree-sitter-templ",
+    "nvim-treesitter/nvim-treesitter-textobjects",
   },
-  build = ':TSUpdate',
+  build = ":TSUpdate",
   config = function()
-    require('nvim-treesitter.configs').setup {
+    require("nvim-treesitter.configs").setup {
       -- Add languages to be installed here that you want installed for treesitter
-      ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'ruby', 'tsx', 'javascript', 'typescript',
-        'vimdoc', 'vim', 'bash', 'terraform' },
+      ensure_installed = { "c", "cpp", "go", "lua", "python", "rust", "ruby", "tsx", "javascript", "typescript",
+        "vimdoc", "vim", "bash", "terraform" },
 
       -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
       auto_install = true,
@@ -19,10 +21,10 @@ return {
       incremental_selection = {
         enable = true,
         keymaps = {
-          init_selection = '<c-space>',
-          node_incremental = '<c-space>',
-          scope_incremental = '<c-s>',
-          node_decremental = '<M-space>',
+          init_selection = "<c-space>",
+          node_incremental = "<c-space>",
+          scope_incremental = "<c-s>",
+          node_decremental = "<M-space>",
         },
       },
       textobjects = {
@@ -31,41 +33,41 @@ return {
           lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
           keymaps = {
             -- You can use the capture groups defined in textobjects.scm
-            ['aa'] = '@parameter.outer',
-            ['ia'] = '@parameter.inner',
-            ['af'] = '@function.outer',
-            ['if'] = '@function.inner',
-            ['ac'] = '@class.outer',
-            ['ic'] = '@class.inner',
+            ["aa"] = "@parameter.outer",
+            ["ia"] = "@parameter.inner",
+            ["af"] = "@function.outer",
+            ["if"] = "@function.inner",
+            ["ac"] = "@class.outer",
+            ["ic"] = "@class.inner",
           },
         },
         move = {
           enable = true,
           set_jumps = true, -- whether to set jumps in the jumplist
           goto_next_start = {
-            [']m'] = '@function.outer',
-            [']]'] = '@class.outer',
+            ["]m"] = "@function.outer",
+            ["]]"] = "@class.outer",
           },
           goto_next_end = {
-            [']M'] = '@function.outer',
-            [']['] = '@class.outer',
+            ["]M"] = "@function.outer",
+            ["]["] = "@class.outer",
           },
           goto_previous_start = {
-            ['[m'] = '@function.outer',
-            ['[['] = '@class.outer',
+            ["[m"] = "@function.outer",
+            ["[["] = "@class.outer",
           },
           goto_previous_end = {
-            ['[M'] = '@function.outer',
-            ['[]'] = '@class.outer',
+            ["[M"] = "@function.outer",
+            ["[]"] = "@class.outer",
           },
         },
         swap = {
           enable = true,
           swap_next = {
-            ['<leader>a'] = '@parameter.inner',
+            ["<leader>a"] = "@parameter.inner",
           },
           swap_previous = {
-            ['<leader>A'] = '@parameter.inner',
+            ["<leader>A"] = "@parameter.inner",
           },
         },
       },
